f7e2d95bac4594404cb7e490f15f436df9d28155
sanjaypbce <www.sanjayijk07@gmail.com>
2025-01-11 17:22:23 +0530

    fixed socket issue

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ const loginRouter = require("./routes/loginjs");
 const messageRouter = require("./routes/messages");
 const userRouter = require("./routes/user");
 const app = express();
-const port = 4000;
-const port2 = 5000;
 const cors = require("cors");
 const client = require("./redis");
-app.use(express.json());
-app.use(cors({ origin: "*" }));
 const http = require("http");
 const { Server } = require("socket.io");
 
-// Create an HTTP server and pass the Express app to it
+const port = process.env.PORT || 4000; // Use Render's dynamic PORT variable
+
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+
+// Create a single HTTP server for both Express and Socket.IO
 const server = http.createServer(app);
 
 // Attach Socket.IO to the HTTP server
@@ -29,6 +30,7 @@ var onlineUsers = [];
 var onlineUserCount = 0;
 var Rooms = [];
 
+// Socket.IO logic
 io.on("connection", (socket) => {
   socket.on("user", (data) => {
     console.log("user connected", data.user._id || data.user.id);
@@ -54,27 +56,20 @@ io.on("connection", (socket) => {
     }
   });
 
-  // Example of emitting an event to a specific room
   socket.on("sendMessageToRoom", ({ roomId, newMessage }) => {
-    // Emit the message to all clients in the specified room
-    console.log("befor", { newMessage }, { roomId });
+    console.log("before", { newMessage }, { roomId });
     io.to(roomId).emit("roomMessage", {
       actualRoomId: roomId,
       message: newMessage,
     });
   });
 
-  // Handle disconnect event
   socket.on("disconnect", () => {
     console.log("user disconnected");
-
-    // Find and remove the user from the onlineUsers array
     const index = onlineUsers.indexOf(socket.id);
     if (index > -1) {
-      onlineUsers.splice(index, 1); // Remove one item only
+      onlineUsers.splice(index, 1);
     }
-
-    // Decrement the online user count
     onlineUserCount -= 1;
     console.log("disconnected", { onlineUsers }, { onlineUserCount });
   });
@@ -88,28 +83,24 @@ const getRoomForUsers = (users) => {
 };
 
 async function connectRedis() {
-  // connecting to redis server
   await client.connect();
 }
+
 connectDb();
 connectRedis();
-// app.use("/home", signinRouter);
-
-app.post("/check", async (req, res) => {
-  const data = await req.body;
-  console.log(data);
-
-  res.send("ok fine");
-});
 
 app.use("/signin", signinRouter);
 app.use("/login", loginRouter);
 app.use("/message", messageRouter);
 app.use("/user", userRouter);
 
-app.listen(port, () => {
-  console.log(`app listening on port ${port}`);
+app.post("/check", async (req, res) => {
+  const data = req.body;
+  console.log(data);
+  res.send("ok fine");
 });
-server.listen(port2, () => {
-  console.log(`server listening on port ${port2}`);
+
+// Start the merged server
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
